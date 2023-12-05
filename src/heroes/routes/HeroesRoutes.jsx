ddb962b21fde5fa70b559b4c73107397fc91bffe
usuarioman <castilla.ar@gmail.com>
2023-12-05 15:18:15 -0600

    Esto es lo mismo

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,17 @@
+import { Navbar } from "../../ui";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Marvel, DC, Search, Hero } from "../pages";
+export const HeroesRoutes = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="marvel" element={<Marvel />} />
+        <Route path="dc" element={<DC />} />
+        <Route path="search" element={<Search />} />
+        <Route path="hero" element={<Hero />} />
+        <Route path="/*" element={<Navigate to={"marvel"} />} />
+      </Routes>
+    </>
+  );
+};
