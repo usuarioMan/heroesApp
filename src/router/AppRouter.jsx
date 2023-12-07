e12233d3676071b4ba0f3d0be421baac2e1073ef
usuarioman <castilla.ar@gmail.com>
2023-12-07 13:24:35 -0600

    add public and private routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,28 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "../auth";
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="/*" element={<HeroesRoutes />} />
+        <Route
+          path="*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
       </Routes>
     </>
   );
---
+++ src/router/PublicRoute.jsx
@@ -0,0 +1,8 @@
+import { AuthContext } from "../auth";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+
+export const PublicRoute = ({ children }) => {
+  const { logged } = useContext(AuthContext);
+  return !logged ? children : <Navigate to={"/marvel"} />;
+};
